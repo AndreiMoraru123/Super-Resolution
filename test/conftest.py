e2ad3a57c7fbf5df7866f21b8b427ffda9792755
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2023-07-21 19:51:49 +0300

    added conftest with GPU restriction

--- test/conftest.py
+++ test/conftest.py
@@ -5,6 +5,7 @@ import tensorflow as tf  # type: ignore
 
 @pytest.fixture(autouse=True)
 def use_cpu_only():
+    """Conv2D and tf.nn.Conv2D are broken on my CUDA/cuDNN version for some reason."""
     os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
     tf.config.set_visible_devices([], 'GPU')
     yield
